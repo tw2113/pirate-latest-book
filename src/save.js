9f27128afe0496cc15e1c9213d06a696f01be44e
Michael Beckwith <tw2113@gmail.com>
2024-04-26 14:05:04 -0500

    we are rendering elsewhere

--- src/save.js
+++ src/save.js
@@ -1,13 +1,4 @@
 import { useBlockProps } from '@wordpress/block-editor';
-export default function save( { attributes } ) {
-	//const {} = attributes;
-
-	const doReturn = true;
-	if ( doReturn ) {
-		return null;
-	}
-
-	return <div { ...useBlockProps.save() }>
-		who
-	</div>;
+export default function save() {
+	return null;
 }
